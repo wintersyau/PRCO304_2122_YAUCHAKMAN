ba0d4410f167075a604bd512c5edfad29fa45d41
chak yau <yauchak2007@yahoo.com.hk>
2022-04-25 21:35:21 +0800

    Base

---
+++ ProtectControl/pch.cpp
@@ -0,0 +1,403 @@
+﻿#pragma once
+
+#include "pch.h"
+
+
+#define KernelListenDrive (L"\\\\.\\ProcessListen")
+
+#define KernelShellDrive (L"\\\\.\\Yueding1007")
+
+#define KernelProtectPPLDrive (L"\\\\.\\Yueding1002")
+
+
+//Superkill.sys 提供内核级对进程的操作 结束进程 挂起恢复进程 以及强制删除文件 和提权等
+
+#define Superkill_NAME "Superkill"
+#define Superkill_PATH "..\\Driver\\Tools\\Superkill.sys"
+
+//ProcessListen.sys 是对SecurityCore的一次大改动他主要是监控新创建的文件
+//之前的SecurityCore是用objectcallback监控进程的但是这个函数可不仅仅是监控进程(我把问题考虑的太简单了)
+//他比预期拿到了更多的数据意味着 我们的软件需要对这些数据进行化验验证安全性但是这是需要时间的
+//但是一个问题产生了objectcallback在没有return的时候目标进程是没法启动的
+//我们在return中间发送数据给软件软件进行检测 虽然我们用了链表但是我们必须要让他
+//对称否则会蓝屏 KeSetEvent 这个函数可以在读取的时候进行加锁也就是读取的时候
+//我们是无法做任何多余的操作的 这会导致一个大问题就是死机 目标进程无法启动
+//如果是其他软件还好如果是系统进程呢？ 很明显我们不能在这个地方检测进程
+//这个sys调用了真正意义的监控进程函数 CreateProcessNotify
+
+#define ProcessListen_NAME "ProcessListen"
+#define ProcessListen_PATH "..\\Driver\\Tools\\ProcessListen.sys"
+
+
+#define ProcessListen_NAME "PPLLProtect"
+#define ProcessListen_PATH "..\\Driver\\Tools\\PPLLProtect.sys"
+
+
+
+BOOL installDvr(LPWSTR drvPath, LPWSTR serviceName) {
+
+    // 打开服务控制管理器数据库
+    SC_HANDLE schSCManager = OpenSCManager(
+        NULL,                   // 目标计算机的名称,NULL：连接本地计算机上的服务控制管理器
+        NULL,                   // 服务控制管理器数据库的名称，NULL：打开 SERVICES_ACTIVE_DATABASE 数据库
+        SC_MANAGER_ALL_ACCESS   // 所有权限
+    );
+    if (schSCManager == NULL) {
+        CloseServiceHandle(schSCManager);
+        return FALSE;
+    }
+
+    // 创建服务对象，添加至服务控制管理器数据库
+    SC_HANDLE schService = CreateService(
+        schSCManager,               // 服务控件管理器数据库的句柄
+        serviceName,                // 要安装的服务的名称
+        serviceName,                // 用户界面程序用来标识服务的显示名称
+        SERVICE_ALL_ACCESS,         // 对服务的访问权限：所有全权限
+        SERVICE_KERNEL_DRIVER,      // 服务类型：驱动服务
+        SERVICE_DEMAND_START,       // 服务启动选项：进程调用 StartService 时启动
+        SERVICE_ERROR_IGNORE,       // 如果无法启动：忽略错误继续运行
+        drvPath,                    // 驱动文件绝对路径，如果包含空格需要多加双引号
+        NULL,                       // 服务所属的负载订购组：服务不属于某个组
+        NULL,                       // 接收订购组唯一标记值：不接收
+        NULL,                       // 服务加载顺序数组：服务没有依赖项
+        NULL,                       // 运行服务的账户名：使用 LocalSystem 账户
+        NULL                        // LocalSystem 账户密码
+    );
+    if (schService == NULL) {
+        CloseServiceHandle(schService);
+        CloseServiceHandle(schSCManager);
+        return FALSE;
+    }
+
+    CloseServiceHandle(schService);
+    CloseServiceHandle(schSCManager);
+    return TRUE;
+}
+
+// 启动服务
+BOOL startDvr(LPWSTR serviceName) {
+
+    // 打开服务控制管理器数据库
+    SC_HANDLE schSCManager = OpenSCManager(
+        NULL,                   // 目标计算机的名称,NULL：连接本地计算机上的服务控制管理器
+        NULL,                   // 服务控制管理器数据库的名称，NULL：打开 SERVICES_ACTIVE_DATABASE 数据库
+        SC_MANAGER_ALL_ACCESS   // 所有权限
+    );
+    if (schSCManager == NULL) {
+        CloseServiceHandle(schSCManager);
+        return FALSE;
+    }
+
+    // 打开服务
+    SC_HANDLE hs = OpenService(
+        schSCManager,           // 服务控件管理器数据库的句柄
+        serviceName,            // 要打开的服务名
+        SERVICE_ALL_ACCESS      // 服务访问权限：所有权限
+    );
+    if (hs == NULL) {
+        CloseServiceHandle(hs);
+        CloseServiceHandle(schSCManager);
+        return FALSE;
+    }
+    if (StartService(hs, 0, 0) == 0) {
+        CloseServiceHandle(hs);
+        CloseServiceHandle(schSCManager);
+        return FALSE;
+    }
+
+
+    CloseServiceHandle(hs);
+    CloseServiceHandle(schSCManager);
+    return TRUE;
+}
+
+// 停止服务
+BOOL stopDvr(LPWSTR serviceName) {
+
+    // 打开服务控制管理器数据库
+    SC_HANDLE schSCManager = OpenSCManager(
+        NULL,                   // 目标计算机的名称,NULL：连接本地计算机上的服务控制管理器
+        NULL,                   // 服务控制管理器数据库的名称，NULL：打开 SERVICES_ACTIVE_DATABASE 数据库
+        SC_MANAGER_ALL_ACCESS   // 所有权限
+    );
+    if (schSCManager == NULL) {
+        CloseServiceHandle(schSCManager);
+        return FALSE;
+    }
+
+    // 打开服务
+    SC_HANDLE hs = OpenService(
+        schSCManager,           // 服务控件管理器数据库的句柄
+        serviceName,            // 要打开的服务名
+        SERVICE_ALL_ACCESS      // 服务访问权限：所有权限
+    );
+    if (hs == NULL) {
+        CloseServiceHandle(hs);
+        CloseServiceHandle(schSCManager);
+        return FALSE;
+    }
+
+    // 如果服务正在运行
+    SERVICE_STATUS status;
+    if (QueryServiceStatus(hs, &status) == 0) {
+        CloseServiceHandle(hs);
+        CloseServiceHandle(schSCManager);
+        return FALSE;
+    }
+    if (status.dwCurrentState != SERVICE_STOPPED &&
+        status.dwCurrentState != SERVICE_STOP_PENDING
+        ) {
+        // 发送关闭服务请求
+        if (ControlService(
+            hs,                         // 服务句柄
+            SERVICE_CONTROL_STOP,       // 控制码：通知服务应该停止
+            &status                     // 接收最新的服务状态信息
+        ) == 0) {
+            CloseServiceHandle(hs);
+            CloseServiceHandle(schSCManager);
+            return FALSE;
+        }
+
+        // 判断超时
+        INT timeOut = 0;
+        while (status.dwCurrentState != SERVICE_STOPPED) {
+            timeOut++;
+            QueryServiceStatus(hs, &status);
+            Sleep(50);
+        }
+        if (timeOut > 80) {
+            CloseServiceHandle(hs);
+            CloseServiceHandle(schSCManager);
+            return FALSE;
+        }
+    }
+
+    CloseServiceHandle(hs);
+    CloseServiceHandle(schSCManager);
+    return TRUE;
+}
+
+// 卸载驱动
+BOOL unloadDvr(LPWSTR serviceName) {
+
+    // 打开服务控制管理器数据库
+    SC_HANDLE schSCManager = OpenSCManager(
+        NULL,                   // 目标计算机的名称,NULL：连接本地计算机上的服务控制管理器
+        NULL,                   // 服务控制管理器数据库的名称，NULL：打开 SERVICES_ACTIVE_DATABASE 数据库
+        SC_MANAGER_ALL_ACCESS   // 所有权限
+    );
+    if (schSCManager == NULL) {
+        CloseServiceHandle(schSCManager);
+        return FALSE;
+    }
+
+    // 打开服务
+    SC_HANDLE hs = OpenService(
+        schSCManager,           // 服务控件管理器数据库的句柄
+        serviceName,            // 要打开的服务名
+        SERVICE_ALL_ACCESS      // 服务访问权限：所有权限
+    );
+    if (hs == NULL) {
+        CloseServiceHandle(hs);
+        CloseServiceHandle(schSCManager);
+        return FALSE;
+    }
+
+    // 删除服务
+    if (DeleteService(hs) == 0) {
+        CloseServiceHandle(hs);
+        CloseServiceHandle(schSCManager);
+        return FALSE;
+    }
+
+    CloseServiceHandle(hs);
+    CloseServiceHandle(schSCManager);
+    return TRUE;
+}
+
+
+
+
+
+char* ConvertLPWSTRToLPSTR(LPWSTR lpwszStrIn)
+{
+	LPSTR pszOut = NULL;
+
+	if (lpwszStrIn != NULL)
+	{
+		int nInputStrLen = wcslen(lpwszStrIn);
+
+		// Double NULL Termination  
+		int nOutputStrLen = WideCharToMultiByte(CP_ACP, 0, lpwszStrIn, nInputStrLen, NULL, 0, 0, 0) + 2;
+		pszOut = new char[nOutputStrLen];
+
+		if (pszOut)
+		{
+			memset(pszOut, 0x00, nOutputStrLen);
+			WideCharToMultiByte(CP_ACP, 0, lpwszStrIn, nInputStrLen, pszOut, nOutputStrLen, 0, 0);
+		}
+	}
+
+
+	return pszOut;
+}
+
+void TC2C(const PTCHAR tc, char* c)
+
+{
+
+#if defined(UNICODE) 
+
+	WideCharToMultiByte(CP_ACP, 0, tc, -1, c, wcslen(tc), 0, 0);
+
+	c[wcslen(tc)] = 0;
+
+#else 
+
+	lstrcpy((PTSTR)c, (PTSTR)tc);
+
+#endif 
+
+
+}
+
+int SendMsgToKernel(LPWSTR pInStr, int len)
+{
+	char* InPutStr = new char[len + 1];
+
+	TC2C(pInStr, InPutStr);
+
+	char* pOutStr = new char[len + 1];
+
+	memset(pOutStr, 0, len + 1);
+
+	int ret_len = 0;
+	// 1.打开驱动设备
+	HANDLE deviceHandle = CreateFile(KernelShellDrive, GENERIC_READ | GENERIC_WRITE,
+		0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM, 0);
+	if (deviceHandle == INVALID_HANDLE_VALUE)
+	{
+		DWORD errCode = ::GetLastError();
+	}
+	else
+	{
+		// 2.向驱动设备发送设备控制请求
+		if (DeviceIoControl(deviceHandle, IOCTL_SEND_AND_REC_STR,
+			InPutStr, len, pOutStr, len + 1, (LPDWORD)&ret_len, NULL))
+		{
+
+			return (int)pOutStr;
+		}
+	}
+
+	return 0;
+}
+
+int SendMsgToKernelByPPL(LPWSTR pInStr, int len)
+{
+	char* InPutStr = new char[len + 1];
+
+	TC2C(pInStr, InPutStr);
+
+	char* pOutStr = new char[len + 1];
+
+	memset(pOutStr, 0, len + 1);
+
+	int ret_len = 0;
+
+	// 1.打开驱动设备
+
+	HANDLE deviceHandle = CreateFile(KernelProtectPPLDrive, GENERIC_READ | GENERIC_WRITE,
+		0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM, 0);
+
+	if (deviceHandle == INVALID_HANDLE_VALUE)
+	{
+		DWORD errCode = ::GetLastError();
+	}
+	else
+	{
+		// 2.向驱动设备发送设备控制请求
+		if (DeviceIoControl(deviceHandle, IOCTL_SEND_AND_REC_STR,
+			InPutStr, len, pOutStr, len + 1, (LPDWORD)&ret_len, NULL))
+		{
+
+			return (int)pOutStr;
+		}
+	}
+
+	return 0;
+}
+
+
+ProcessCallbackFunction OneProcessAction = NULL;
+
+void SetProcessCallback(ProcessCallbackFunction OneCall)
+{
+	OneProcessAction = OneCall;
+}
+
+HANDLE ProcessListenDrive = NULL;
+
+BOOL StartProcessListenService(int Check)
+{
+	if (Check == 1)
+	{
+		if (ProcessListenDrive == NULL)
+		{
+			ULONG bytesReturned;
+			/*打开设备，用我们自定的符号链接，响应驱动IRP_MJ_CREATE*/
+			ProcessListenDrive = CreateFile(KernelListenDrive,
+				GENERIC_READ | GENERIC_WRITE,
+				0,
+				0,
+				OPEN_EXISTING,
+				FILE_ATTRIBUTE_NORMAL,
+				0);
+
+			if (ProcessListenDrive == INVALID_HANDLE_VALUE)
+			{
+				return FALSE;
+			}
+
+			return TRUE;
+		}
+	}
+	else
+	{
+		if (ProcessListenDrive != NULL)
+		{
+			CloseHandle(ProcessListenDrive);
+			ProcessListenDrive = NULL;
+		}
+	}
+	
+
+	return FALSE;
+}
+
+void ProcessListenServiceLoop()
+{
+	DWORD ReturnLength = 0;
+	PROCESSINFO PLItem;
+
+	memset(&PLItem, 0, sizeof(PROCESSINFO));
+
+	BOOL SendState = DeviceIoControl(
+		ProcessListenDrive,
+		CWK_DVC_RECV_STR,
+		0,
+		0,
+		&PLItem,
+		sizeof(PLItem),
+		&ReturnLength,
+		0
+	);
+
+	if (OneProcessAction != NULL)
+	{
+		OneProcessAction((ULONG)PLItem.hParentProcessId, (ULONG)PLItem.hProcessId, PLItem.time.wHour,PLItem.time.wMinute,PLItem.time.wSecond,PLItem.time.wMilliseconds,PLItem.ParentSystem,PLItem.ThisSystem);
+	}
+
+}
+
+
---
+++ ProtectControl/pch.h
@@ -0,0 +1,152 @@
+﻿// pch.h: 这是预编译标头文件。
+// 下方列出的文件仅编译一次，提高了将来生成的生成性能。
+// 这还将影响 IntelliSense 性能，包括代码完成和许多代码浏览功能。
+// 但是，如果此处列出的文件中的任何一个在生成之间有更新，它们全部都将被重新编译。
+// 请勿在此处添加要频繁更新的文件，这将使得性能优势无效。
+
+#define PCH_H
+
+// 添加要在此处预编译的标头
+#include "framework.h"
+#include<windows.h>
+#include<string>
+
+
+// 设备名对应的符号链接名，用于暴露给应用层。符号链接在应用看来是在\\.\ 的
+
+
+
+using namespace std;
+
+#ifdef _UNICODE
+#define tcout wcout
+#define tcerr wcerr
+#else
+#define tcout cout
+#define tcerr cerr
+#endif
+
+
+#define FILE_READ_DATA            ( 0x0001 )    // file & pipe
+#define FILE_LIST_DIRECTORY       ( 0x0001 )    // directory
+#define FILE_WRITE_DATA           ( 0x0002 )    // file & pipe
+#define FILE_ADD_FILE             ( 0x0002 )    // directory
+#define FILE_APPEND_DATA          ( 0x0004 )    // file
+#define FILE_ADD_SUBDIRECTORY     ( 0x0004 )    // directory
+#define FILE_CREATE_PIPE_INSTANCE ( 0x0004 )    // named pipe
+#define FILE_READ_EA              ( 0x0008 )    // file & directory
+#define FILE_WRITE_EA             ( 0x0010 )    // file & directory
+#define FILE_EXECUTE              ( 0x0020 )    // file
+#define FILE_TRAVERSE             ( 0x0020 )    // directory
+#define FILE_DELETE_CHILD         ( 0x0040 )    // directory
+#define FILE_READ_ATTRIBUTES      ( 0x0080 )    // all
+#define FILE_WRITE_ATTRIBUTES     ( 0x0100 )    // all
+#define FILE_ALL_ACCESS (STANDARD_RIGHTS_REQUIRED | SYNCHRONIZE | 0x1FF)
+#define FILE_DEVICE_SCANNER             0x00000019
+#define FILE_DEVICE_SERIAL_MOUSE_PORT   0x0000001a
+#define FILE_DEVICE_SERIAL_PORT         0x0000001b
+#define FILE_DEVICE_SCREEN              0x0000001c
+#define FILE_DEVICE_SOUND               0x0000001d
+#define FILE_DEVICE_STREAMS             0x0000001e
+#define FILE_DEVICE_TAPE                0x0000001f
+#define FILE_DEVICE_TAPE_FILE_SYSTEM    0x00000020
+#define FILE_DEVICE_TRANSPORT           0x00000021
+#define FILE_DEVICE_UNKNOWN             0x00000022
+#define FILE_DEVICE_VIDEO               0x00000023
+#define FILE_DEVICE_VIRTUAL_DISK        0x00000024
+#define FILE_DEVICE_WAVE_IN             0x00000025
+#define FILE_DEVICE_WAVE_OUT            0x00000026
+#define FILE_DEVICE_8042_PORT           0x00000027
+#define FILE_DEVICE_NETWORK_REDIRECTOR  0x00000028
+#define FILE_DEVICE_BATTERY             0x00000029
+#define FILE_DEVICE_BUS_EXTENDER        0x0000002a
+#define FILE_DEVICE_MODEM               0x0000002b
+#define FILE_DEVICE_VDM                 0x0000002c
+#define FILE_DEVICE_MASS_STORAGE        0x0000002d
+#define FILE_DEVICE_SMB                 0x0000002e
+#define FILE_DEVICE_KS                  0x0000002f
+#define METHOD_BUFFERED                 0
+#define METHOD_IN_DIRECT                1
+#define METHOD_OUT_DIRECT               2
+#define METHOD_NEITHER                  3
+
+
+#define CTL_CODE( DeviceType, Function, Method, Access ) (                 \
+    ((DeviceType) << 16) | ((Access) << 14) | ((Function) << 2) | (Method) \
+)
+
+typedef void (*ProcessCallbackFunction)(const ULONG A, ULONG B, unsigned short C, unsigned short D, unsigned short E, unsigned short F,BOOLEAN G, BOOLEAN H);
+
+
+//CTL_CODE创建控制码
+#define IOCTL_SEND_AND_REC_STR\
+	CTL_CODE(FILE_DEVICE_UNKNOWN\
+	, 0x801, METHOD_BUFFERED,\
+	FILE_READ_DATA | FILE_WRITE_DATA)
+
+
+#pragma region ProcessListenHead
+
+#define OPEN_EXISTING       3
+
+
+#define  CWK_DVC_RECV_STR \
+	(ULONG)CTL_CODE( \
+	FILE_DEVICE_UNKNOWN, \
+	0x912,METHOD_BUFFERED, \
+	FILE_READ_DATA)
+
+
+typedef struct _TIME_FIELDS {
+    WORD wYear;
+    WORD wMonth;
+    WORD wDay;
+    WORD wHour;
+    WORD wMinute;
+    WORD wSecond;
+    WORD wMilliseconds;
+    WORD wWeekday;
+} TIME_FIELDS;
+
+typedef TIME_FIELDS* PTIME_FIELDS;
+
+typedef struct _PROCESSINFO
+{
+	TIME_FIELDS			time;						// 时间
+	BOOLEAN				bIsCreate;					// 是否是创建进程
+	HANDLE				hParentProcessId;			// 父进程 ID
+	ULONG				ulParentProcessLength;		// 父进程长度
+	HANDLE				hProcessId;					// 子进程 ID
+	ULONG				ulProcessLength;			// 子进程长度
+	ULONG				ulCommandLineLength;		// 进程命令行参数长度
+	BOOLEAN             ParentSystem;
+	BOOLEAN             ThisSystem;
+	UCHAR				uData[1];					// 数据域
+} PROCESSINFO, * PPROCESSINFO;
+#pragma endregion
+
+
+
+
+extern "C"
+{
+	_declspec(dllexport) int SendMsgToKernel(LPWSTR pInStr, int len);
+
+	_declspec(dllexport) int SendMsgToKernelByPPL(LPWSTR pInStr, int len);
+
+	_declspec(dllexport) void SetProcessCallback(ProcessCallbackFunction OneCall);
+
+    _declspec(dllexport) BOOL StartProcessListenService(int Check);
+
+    _declspec(dllexport) void ProcessListenServiceLoop();
+
+	_declspec(dllexport) BOOL installDvr(LPWSTR drvPath, LPWSTR serviceName);
+
+	_declspec(dllexport) BOOL startDvr(LPWSTR serviceName);
+
+	_declspec(dllexport) BOOL stopDvr(LPWSTR serviceName);
+
+	_declspec(dllexport) BOOL unloadDvr(LPWSTR serviceName);
+};
+
+
